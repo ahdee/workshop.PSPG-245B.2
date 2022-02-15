0c144d0a22fd978aeb94834bd68c003b68810853
ahdee <simplitia@gmail.com>
2022-02-14 18:40:24 -0800

    lecture_updates

--- bonus.R
+++ bonus.R
@@ -1,1 +1,4 @@
-nrow ( studies[ grepl("Lung Cancer", studies$name, ignore.case = T ), ] )
+dim ( studies[ grepl("Breast Invasive Carcinoma", studies$name, ignore.case = T )  , ]  )
+
+# X how would you get only samples labeled as 	Her2-positive breast tumors
+mycaselist[ mycaselist$case_list_id == "brca_tcga_pub2015_her2pos", ]$case_ids
