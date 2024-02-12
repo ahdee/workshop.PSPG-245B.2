754e03b35519ee0c93eba3a46b946135427c6c4e
ahdee <simplitia@gmail.com>
2024-02-11 18:09:12 -0800

    cleanup

--- bonus.R
+++
@@ -1,4 +0,0 @@
-dim ( studies[ grepl("Breast Invasive Carcinoma", studies$name, ignore.case = T )  , ]  )
-
-# X how would you get only samples labeled as 	Her2-positive breast tumors
-mycaselist[ mycaselist$case_list_id == "brca_tcga_pub2015_her2pos", ]$case_ids
